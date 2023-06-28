6ac9f84dc27a26bf97a1fb08fabd90ce1c134921
Patrick Haas <patmanteau@posteo.de>
2023-06-28 22:14:31 +0200

    Move scrapers

---
+++ wdscrape/wdscrape/pipelines.py
@@ -0,0 +1,21 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import jsonlines as jl
+
+
+class WdscrapePipeline:
+    def open_spider(self, spider):
+        self.jl_file = jl.open("../out/wd-items.jsonl", "a")
+
+    def close_spider(self, spider):
+        self.jl_file.close()
+
+    def process_item(self, item, spider):
+        self.jl_file.write({item["id"]: {"wikidata": item}})
+        return item
---
+++ wdscrape/wdscrape/spiders/wdspider.py
@@ -0,0 +1,51 @@
+import scrapy
+import json
+import jsonlines as jl
+
+
+class WdspiderSpider(scrapy.Spider):
+    name = "wdspider"
+    allowed_domains = ["www.wikidata.org"]
+    # start_urls = ["https://www.wikidata.org/wiki/Special:EntityData/Q42.json"]
+
+    def start_requests(self):
+        existing = []
+
+        with jl.open("../out/wd-items.jsonl", "r") as wd_items:
+            existing = [
+                list(item.keys())[0]
+                for item in wd_items.iter(type=dict, skip_invalid=True)
+            ]
+
+        with jl.open("../out/catalog.jsonl", "r") as req_items:
+            for item in req_items:
+                if item["id"] not in existing:
+                    yield scrapy.Request(
+                        url=f"https://www.wikidata.org/wiki/Special:EntityData/{item['id']}.json",
+                        callback=self.parse,
+                        cb_kwargs=dict(entity_id=item["id"]),
+                    )
+
+    def parse(self, response, entity_id):
+        res = json.loads(response.body)
+        item = {}
+        item["url"] = response.url
+        item["id"] = entity_id
+
+        # get correct id if redirected
+        id_ = list(res["entities"].keys())[0]
+        if id_ != entity_id:
+            item["redirect"] = id_
+
+        item["label"] = (
+            res["entities"][id_]["labels"]["en"]["value"]
+            if res["entities"][id_]["labels"].get("en")
+            else id_
+        )
+        item["description"] = (
+            res["entities"][id_]["descriptions"]["en"]["value"]
+            if res["entities"][id_]["descriptions"].get("en")
+            else id_
+        )
+
+        yield item
